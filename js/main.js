98e75996d74eb0da1f4b5a4f3ee09c8186fbb4b7
DikiiPokemon <snowflackssss@mail.ru>
2022-08-23 23:54:42 +0300

    Animation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 
 $(window).scroll(function(){
     if ($(window).scrollTop() > 1) { //если страница прокручена
-      $('#Header').css('opacity', '0.4')
+      $('#Header').css({'background': 'rgba(255, 255, 255, 0.4)',
+      "backdrop-filter": "blur(20px)"
+    })
     } else {//иначе
-        $('#Header').css('opacity', '1')
+        $('#Header').css({
+          'background': '#FFFFFF',
+          "backdrop-filter": "none"
+        })
     }
  });
+
+ $(window).scroll(function(){
+  if ($(window).scrollTop() > parseInt($('#LastHeader').css('top'))) { //если страница прокручена
+    /*$('#JU').css('display', 'none')*/
+    $('#JU').fadeOut(250);
+  } else {//иначе
+    /*$('#JU').css('display', 'block')*/
+    $('#JU').fadeIn(250);
+  }
+});
+
+console.log($('#LastHeader').css('top'))
