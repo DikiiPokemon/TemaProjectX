1404a12d82745afa552d9b005a374399755e80cd
DikiiPokemon <snowflackssss@mail.ru>
2022-09-07 00:58:56 +0300

    NaN

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var scrollingBool;
 var slideCount; //for Slider
 var mobileBool;
-
+var slideInterval = 10000;
+var switchInterval
 
 function MobileYesOrNo(){
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
@@ -19,9 +20,9 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 }
 
   
-  scrollingBool = false;
-  console.log(mobileBool)// код для мобильных устройств
+  scrollingBool = false;// код для мобильных устройств
 } else {
+  switchInterval = setInterval(nextSlide, slideInterval);
   $('.header').css('position', 'fixed');
     slideCount = $('#Desktop').children().length;
     scrollingBool = true;
@@ -60,7 +61,6 @@ $('#BackTop').on('click', function(e) {
 });
 
 
-console.log($('#LastHeader').css('top'))
 
 //Slider
 var IdRed; //id of the tag navigation button, that active
@@ -69,7 +69,6 @@ var slideNow = 1;
 var previousSlide;//number of the previous slide
 
 
-console.log(slideCount);
 
 function nextSlide() {
   if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
@@ -95,19 +94,18 @@ function nextSlide() {
       IdWhite = mobileBool + `Button` + previousSlide;
       $(IdWhite).css('background', '#fff');
       $(IdRed).css('background', '#D12A32');
-      console.log(translateWidth)
   }
 
   
 }
 
-var slideInterval = 10000;
+
 var BurgerBool = 0;
 var translateWidth = 0;
 
 //Clear and Set Interval of listing slider, burger menu
 $(document).ready(function () {
-  var switchInterval = setInterval(nextSlide, slideInterval);
+  
   MobileYesOrNo();
 
   (function () {
@@ -160,7 +158,6 @@ $(document).ready(function () {
   }
 
   IdRed = mobileBool + `Button` + slideNow;  
-  console.log(IdRed)
   $(IdRed).css('background', '#D12A32');
 
   $('.thirdHeader').hover(function(){
@@ -248,25 +245,30 @@ $('.ButtonJoinUs').click(function(){
 
 $('#ButtonPrevious').click(function() {
   prevSlide();
-  console.log("click")
 });
 
 $('#ButtonNext').click(function() {
   nextSlide();
-  console.log("click")
 });
 
 $('#MButtonPrevious').click(function() {
   prevSlide();
-  console.log("click")
 });
 
 $('#MButtonNext').click(function() {
   nextSlide();
-  console.log("click")
 });
 
+$( ".SlideWrapper" ).on( "swipeleft", swipeleftHandler );
+$( ".SlideWrapper" ).on( "swiperight", swiperightHandler );
 
+function swipeleftHandler(){
+  nextSlide();
+}
+
+function swiperightHandler(){
+  prevSlide();
+}
 //Navigation Buttons
 var navBtnId = 0;
 
@@ -287,3 +289,126 @@ $('.NavButton').click(function() {
     slideNow = navBtnId + 1;
   }
 });
+
+var triggerElementID = null; // this variable is used to identity the triggering element
+var fingerCount = 0;
+var startX = 0;
+var startY = 0;
+var curX = 0;
+var curY = 0;
+var deltaX = 0;
+var deltaY = 0;
+var horzDiff = 0;
+var vertDiff = 0;
+var minLength = 72; // the shortest distance the user may swipe
+var swipeLength = 0;
+var swipeAngle = null;
+var swipeDirection = null;
+
+// The 4 Touch Event Handlers
+
+// NOTE: the touchStart handler should also receive the ID of the triggering element
+// make sure its ID is passed in the event call placed in the element declaration, like:
+// <div id="picture-frame" ontouchstart="touchStart(event,'picture-frame');"  ontouchend="touchEnd(event);" ontouchmove="touchMove(event);" ontouchcancel="touchCancel(event);">
+
+function touchStart(event,passedName) {
+    // disable the standard ability to select the touched object
+    event.preventDefault();
+    // get the total number of fingers touching the screen
+    fingerCount = event.touches.length;
+    // since we're looking for a swipe (single finger) and not a gesture (multiple fingers),
+    // check that only one finger was used
+    if ( fingerCount == 1 ) {
+        // get the coordinates of the touch
+        startX = event.touches[0].pageX;
+        startY = event.touches[0].pageY;
+        // store the triggering element ID
+        triggerElementID = passedName;
+    } else {
+        // more than one finger touched so cancel
+        touchCancel(event);
+    }
+}
+
+function touchMove(event) {
+    event.preventDefault();
+    if ( event.touches.length == 1 ) {
+        curX = event.touches[0].pageX;
+        curY = event.touches[0].pageY;
+    } else {
+        touchCancel(event);
+    }
+}
+
+function touchEnd(event) {
+    event.preventDefault();
+    // check to see if more than one finger was used and that there is an ending coordinate
+    if ( fingerCount == 1 && curX != 0 ) {
+        // use the Distance Formula to determine the length of the swipe
+        swipeLength = Math.round(Math.sqrt(Math.pow(curX - startX,2) + Math.pow(curY - startY,2)));
+        // if the user swiped more than the minimum length, perform the appropriate action
+        if ( swipeLength >= minLength ) {
+            caluculateAngle();
+            determineSwipeDirection();
+            processingRoutine();
+            touchCancel(event); // reset the variables
+        } else {
+            touchCancel(event);
+        }   
+    } else {
+        touchCancel(event);
+    }
+}
+
+function touchCancel(event) {
+    // reset the variables back to default values
+    fingerCount = 0;
+    startX = 0;
+    startY = 0;
+    curX = 0;
+    curY = 0;
+    deltaX = 0;
+    deltaY = 0;
+    horzDiff = 0;
+    vertDiff = 0;
+    swipeLength = 0;
+    swipeAngle = null;
+    swipeDirection = null;
+}
+
+function caluculateAngle() {
+    var X = startX-curX;
+    var Y = curY-startY;
+    var Z = Math.round(Math.sqrt(Math.pow(X,2)+Math.pow(Y,2))); //the distance - rounded - in pixels
+    var r = Math.atan2(Y,X); //angle in radians (Cartesian system)
+    swipeAngle = Math.round(r*180/Math.PI); //angle in degrees
+    if ( swipeAngle < 0 ) { swipeAngle =  360 - Math.abs(swipeAngle); }
+}
+
+function determineSwipeDirection() {
+
+    if ( (swipeAngle <= 45) && (swipeAngle >= 0) ) {
+        swipeDirection = 'left';
+    } else if ( (swipeAngle <= 360) && (swipeAngle >= 315) ) {
+        swipeDirection = 'left';
+    } else if ( (swipeAngle >= 135) && (swipeAngle <= 225) ) {
+        swipeDirection = 'right';
+    } else if ( (swipeAngle > 45) && (swipeAngle < 135) ) {
+        swipeDirection = 'down';
+    } else {
+        swipeDirection = 'up';
+    }
+}
+
+function processingRoutine() {
+    var swipedElement = document.getElementById('#Mobile');
+    if ( swipeDirection == 'left' ) {
+        nextSlide();         
+    } else if ( swipeDirection == 'right' ) {
+        prevSlide();
+    } else if ( swipeDirection == 'up' ) {
+        prevSlide();         
+    } else if ( swipeDirection == 'down' ) {
+        nextSlide();         
+    }
+}
